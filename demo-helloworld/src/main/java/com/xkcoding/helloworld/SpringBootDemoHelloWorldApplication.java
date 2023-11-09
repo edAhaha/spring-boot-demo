4f581091b462933f4c6bbae9dadac048d12e5e82
xtwang <iextwang@163.com>
2023-11-09 15:33:53 +0800

    HelloWorld 优化

--- demo-helloworld/src/main/java/com/xkcoding/helloworld/SpringBootDemoHelloWorldApplication.java
+++ demo-helloworld/src/main/java/com/xkcoding/helloworld/SpringBootDemoHelloWorldApplication.java
@@ -17,23 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
-public class SpringBootDemoHelloworldApplication {
+public class SpringBootDemoHelloWorldApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoHelloworldApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootDemoHelloWorldApplication.class, args);
+  }
 
-    /**
-     * Hello，World
-     *
-     * @param who 参数，非必须
-     * @return Hello, ${who}
-     */
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(required = false, name = "who") String who) {
-        if (StrUtil.isBlank(who)) {
-            who = "World";
-        }
-        return StrUtil.format("Hello, {}!", who);
+  /**
+   * Hello，World
+   *
+   * @param who 参数，非必须
+   * @return Hello, ${who}
+   */
+  @GetMapping("/hello")
+  public String sayHello(@RequestParam(required = false, name = "who") String who) {
+    if (StrUtil.isBlank(who)) {
+      who = "World";
     }
+    return StrUtil.format("Hello, {}!", who);
+  }
 }
--- demo-helloworld/src/test/java/com/xkcoding/helloworld/SpringBootDemoHelloWorldApplicationTests.java
+++ demo-helloworld/src/test/java/com/xkcoding/helloworld/SpringBootDemoHelloWorldApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootDemoHelloworldApplicationTests {
+public class SpringBootDemoHelloWorldApplicationTests {
 
     @Test
     public void contextLoads() {
